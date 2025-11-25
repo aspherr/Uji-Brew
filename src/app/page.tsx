040bcf99c896f56a6c227286595e5ee309cf17ec
aspherr <huzaifa.syedd@gmail.com>
2025-11-25 12:11:45 +0000

    feat: created the 'brewing / lifestyle' section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -114,6 +114,47 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      <section className="mt-40 mb-20"> 
+        <div className="flex flex-row max-w-4xl mx-auto mt-10 gap-15">
+          <div className="relative shrink-0 h-150">
+            <Image
+              src="/matcha-ritual.jpg"
+              width={400}
+              height={800}
+              alt="matcha ceremony"
+              className="rounded shadow-sm block"
+            />
+
+            <div className="pointer-events-none animate-pulse h-3 w-3 rounded-full bg-green-500 opacity-20 absolute -right-1 -top-1"></div>
+            <div className="pointer-events-none animate-pulse h-3 w-3 rounded-full bg-green-500 opacity-20 absolute -right-1 -bottom-1"></div>
+            <div className="pointer-events-none animate-pulse h-3 w-3 rounded-full bg-green-500 opacity-20 absolute -left-1 -top-1"></div>
+            <div className="pointer-events-none animate-pulse h-3 w-3 rounded-full bg-green-500 opacity-20 absolute -left-1 -bottom-1"></div>
+          </div>
+
+          <div className="flex flex-col items-center font-sans -mt-1">
+            <h1 className="font-bold text-4xl">THE RITUAL OF MATCHA</h1>
+
+            <div className="text-gray-600 mt-3 ml-2 space-y-4 leading-relaxed">
+              <p>
+                Our matcha is sourced from the renowned tea fields of Uji, Japan, a region celebrated for producing some of the finest matcha in the world. For centuries, farmers in Uji have refined the art of cultivating exceptional tea, passing down techniques that ensure unmatched quality and flavour.
+              </p>
+
+              <p>
+                Before harvest, the tea plants are carefully shade-grown for several weeks, allowing the leaves to develop their vibrant green colour and naturally sweet, umami-rich taste. This process increases the concentration of nutrients and antioxidants that make matcha both delicious and nourishing.
+              </p>
+
+              <p>
+                Once harvested, the leaves are gently processed and stone-ground using traditional granite mills, slowly transforming them into the fine powder known as matcha. This meticulous process preserves the tea’s delicate aroma, smooth texture, and vivid colour.
+              </p>
+
+              <p>
+                Every cup reflects a timeless Japanese ritual — a moment of calm, craftsmanship, and connection to centuries of tea tradition.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
