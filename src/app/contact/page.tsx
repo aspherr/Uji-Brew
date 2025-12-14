cb9bc1fd554a0013df78043d1e60f0a5b9d79cb3
aspherr <huzaifa.syedd@gmail.com>
2025-12-14 12:21:39 +0000

    feat: created the contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className="w-full min-h-screen font-sans">
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Page
