cdfb0c999bf385c965bb5451800db0139dad7ff8
aspherr <huzaifa.syedd@gmail.com>
2025-12-23 20:22:25 +0000

    feat: created product cards for products page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,21 +1,39 @@
 import Image from "next/image";
 
 import Navbar from "@/components/Navbar";
+import ProductCard from "@/components/ProductCard";
+import { getProducts } from "@/services/services.products";
+import Footer from "@/components/Footer";
 
-const Page = () => {
-
-  return (
+const Page = async () => {
+    const products = await getProducts();
+    
+    return (
+    
     <div className="w-full min-h-screen font-sans">
         <section>
             <div className="mx-auto max-w-8xl -my-7 mb-0.5">
-                <Navbar/>
+                <Navbar />
+            </div>
+        </section>
+
+        <section className="bg-green-100 py-20">
+                <div className="flex flex-col items-center justify-center font-sans w-full max-w-7xl mx-auto">
+                <h1 className="font-bold text-4xl">DISCOVER OUR MATCHA SELECTION</h1>
+                <p className="mt-2 text-gray-700 text-xl"></p>
+            
+                <div className="grid grid-cols-3 mt-10 gap-15">
+                    {products.map((product) => (
+                        <div key={product.id}>
+                            <ProductCard product={product} />
+                        </div> 
+                    ))}
+                </div>
             </div>
         </section>
 
         <section>
-            <div>
-                
-            </div>
+            <Footer />
         </section>
     </div>
   )
---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image"
+
+import { Check, Plus } from 'lucide-react';
+
+  type Product = {
+    id: string
+    name: string
+    desc: string | null
+    price: number
+    imageUrl: string | null
+    stock: number
+    isAvailable: boolean
+  }
+
+  export default function ProductCard({ product }: { product: Product }) {
+    return (
+    <div className="w-80 h-120 bg-white shadow-md rounded-xl border-2 flex flex-col">
+        <div className="w-full h-3/5 rounded-t-xl flex items-center justify-center">
+            <Image src={product.imageUrl ?? ""} width={225} height={225} alt="menu item" />
+        </div>
+
+        <div className="mt-3 px-4">
+            <h1 className="text-xl font-bold">{product.name}</h1>
+            <p className="text-xs mt-1 line-clamp-2">{product.desc}</p>
+        </div>
+
+        <div className="flex items-center justify-between w-full px-4 mt-auto mb-5">
+            <h3 className="font-bold text-xl whitespace-nowrap">
+                £{(product.price / 100).toFixed(2)}
+            </h3>
+
+            <div className="flex items-center justify-center gap-3">
+                <div className="size-7 min-w-7 min-h-7 rounded-full bg-green-300 hover:bg-green-400 transition-all duration-300 flex items-center justify-center shrink-0 overflow-hidden">
+                    <Check className="w-3.5 h-3.5" />
+                </div>
+
+                <button
+                    type="button"
+                    className="size-7 min-w-7 min-h-7 rounded-full bg-green-300 hover:bg-green-400 transition-all duration-300 flex items-center justify-center shrink-0 overflow-hidden p-0"
+                >
+                    <Plus className="w-3.5 h-3.5" />
+                </button>
+            </div>
+        </div>
+    </div>
+    );
+  }
