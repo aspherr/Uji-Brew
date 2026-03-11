6eba2d7090d5e59a63b831c34e667be214074c55
aspherr <huzaifa.syedd@gmail.com>
2026-03-11 19:07:01 +0000

    deps: installed shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
-import Navbar from "./components/Navbar";
+import Navbar from "../components/Navbar";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -13,9 +14,9 @@ export default function Home() {
         <div className="font-sans ml-20">
           <h1 className="font-bold text-5xl leading-tight">Experience the Taste of Authentic Japanese <span className="text-green-500">Matcha</span></h1>
           <p className="mt-1 text-xl text-gray-700">Stone-ground green tea crafted for purity and balance.</p>
-          <button className="mt-6 bg-green-500 text-white font-semibold px-6 py-3 rounded-md hover:bg-green-600 duration-300 text-md">
+          <Button className="mt-6 p-5 font-semibold text-md bg-green-500 hover:bg-green-600 duration-300">
             Explore Now
-          </button>
+          </Button>
         </div>
 
         <Image src="/matcha-splash.svg" width={600} height={600} alt="matcha cup splash art"/>
