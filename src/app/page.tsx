1cb9f152127a79d71d4e798ca88e6ad4d435a552
aspherr <huzaifa.syedd@gmail.com>
2026-03-12 17:45:10 +0000

    feat: created a 'why choose us' section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
+import { Leaf, MountainSnow, Zap } from "lucide-react"
+
 import { Button } from "@/components/ui/button";
 
 import Navbar from "../components/Navbar";
@@ -8,51 +10,110 @@ import Card from "@/components/Card";
 export default function Home() {
   return (
     <div className="w-full min-h-screen">
-      <div className="mx-auto max-w-8xl -my-7 mb-0.5">
-        <Navbar/>
-      </div>
-
-      <div className="flex items-center justify-between max-w-7xl mx-auto">
-        <div className="font-sans ml-20">
-          <h1 className="font-bold text-5xl leading-tight">Experience the Taste of Authentic Japanese <span className="text-green-500">Matcha</span></h1>
-          <p className="mt-1 text-xl text-gray-700">Stone-ground green tea crafted for purity and balance.</p>
-          <Button className="mt-6 p-5 font-semibold text-md bg-green-500 hover:bg-green-600 duration-300">
-            Explore Now
-          </Button>
+      <section>
+        <div className="mx-auto max-w-8xl -my-7 mb-0.5">
+          <Navbar/>
         </div>
+      </section>
 
-        <Image src="/matcha-splash.svg" width={600} height={600} alt="matcha cup splash art"/>
-      </div>
+      <section>
+        <div className="flex items-center justify-between max-w-7xl mx-auto">
+          <div className="font-sans ml-20">
+            <h1 className="font-bold text-5xl leading-tight">Experience the Taste of Authentic Japanese <span className="text-green-500">Matcha</span></h1>
+            <p className="mt-1 text-xl text-gray-700">Stone-ground green tea crafted for purity and balance.</p>
+            <Button className="mt-6 p-5 font-semibold text-md bg-green-500 hover:bg-green-600 duration-300">
+              Explore Now
+            </Button>
+          </div>
 
-      <div className="flex flex-col items-center justify-center font-sans mt-10">
-        <h1 className="font-bold text-4xl">FEATURED MATCHA</h1>
-        <p className="mt-2 text-gray-700 text-xl">
-          Discover our most <span className="underline decoration-green-500 decoration-2 underline-offset-5">popular</span> matcha selections.
-        </p>
-
-        <div className="grid grid-cols-3 mt-10 gap-10">
-          <Card 
-            name="ICED CLOUD MATCHA LATTE" 
-            desc="Smooth ceremonial matcha topped with a light, creamy cloud foam." 
-            price="£7.00"
-            image="/featured/item-1.svg"
-          />
-
-          <Card 
-            name="ICED STRAWBERRY MATCHA LATTE" 
-            desc="Fresh strawberry sweetness layered with vibrant iced matcha." 
-            price="£7.00"
-            image="/featured/item-2.svg"
-          />
-
-          <Card 
-            name="ICED CARAMEL MATCHA LATTE" 
-            desc="Rich caramel blended with smooth matcha over refreshing ice." 
-            price="£7.00"
-            image="/featured/item-3.svg"
-          />
+          <Image src="/matcha-splash.svg" width={600} height={600} alt="matcha cup splash art"/>
         </div>
-      </div>
+      </section>
+
+      <section className="mt-40">
+        <div className="flex flex-col items-center justify-center font-sans">
+          <h1 className="font-bold text-4xl">FEATURED MATCHA</h1>
+          <p className="mt-2 text-gray-700 text-xl">
+            Discover our most <span className="underline decoration-green-500 decoration-2 underline-offset-5">popular</span> matcha selections.
+          </p>
+
+          <div className="grid grid-cols-3 mt-10 gap-10">
+            <Card 
+              name="ICED CLOUD MATCHA LATTE" 
+              desc="Smooth ceremonial matcha topped with a light, creamy cloud foam." 
+              price="£7.00"
+              image="/featured/item-1.svg"
+            />
+
+            <Card 
+              name="ICED STRAWBERRY MATCHA LATTE" 
+              desc="Fresh strawberry sweetness layered with vibrant iced matcha." 
+              price="£7.00"
+              image="/featured/item-2.svg"
+            />
+
+            <Card 
+              name="ICED CARAMEL MATCHA LATTE" 
+              desc="Rich caramel blended with smooth matcha over refreshing ice." 
+              price="£7.00"
+              image="/featured/item-3.svg"
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className="mt-40 mb-20">
+        <div className="flex flex-col items-center justify-center font-sans">
+          <h1 className="font-bold text-4xl">WHAT MAKES OUR MATCHA SPECIAL</h1>
+
+          <div className="grid grid-cols-3 mt-15 gap-10 max-w-7xl mx-auto">
+            <div className="flex flex-col items-center gap-5">
+              <div className="flex items-center justify-center p-4 rounded-full bg-green-200">
+                <MountainSnow size={35}/>
+              </div>
+
+              <div className="flex flex-col items-center justify-center">
+                <h1 className="font-bold text-2xl">
+                  Stone-Ground in Japan
+                </h1>
+                <p className="text-center max-w-xs text-gray-600 mt-1">
+                  Traditionally stone-milled matcha sourced from Uji, Japan — the heart of premium Japanese tea.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center gap-5">
+              <div className="flex items-center justify-center p-4 rounded-full bg-green-200">
+                <Leaf size={35}/>
+              </div>
+
+              <div className="flex flex-col items-center justify-center">
+                <h1 className="font-bold text-2xl">
+                  Premium Tea Leaves
+                </h1>
+                <p className="text-center max-w-xs text-gray-600 mt-1">
+                  Carefully shade-grown leaves produce matcha with a smooth taste and vibrant green colour.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center gap-5">
+              <div className="flex items-center justify-center p-4 rounded-full bg-green-200">
+                <Zap size={35}/>
+              </div>
+
+              <div className="flex flex-col items-center justify-center">
+                <h1 className="font-bold text-2xl">
+                  Natural Energy
+                </h1>
+                <p className="text-center max-w-xs text-gray-600 mt-1">
+                  Rich in antioxidants and natural caffeine for calm, sustained energy throughout the day.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
